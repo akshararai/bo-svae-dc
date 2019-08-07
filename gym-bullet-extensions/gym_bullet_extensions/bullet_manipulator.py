fixed sawyer orientation, badlen calculation still wrong

gym-bullet-extensions/gym_bullet_extensions/bullet_manipulator.py
@@ -280,7 +280,10 @@ class BulletManipulator:
 
     def get_max_fing_dist(self):
         farr = np.array(self.info.finger_jids_lst)
-        return self.info.joint_maxpos[farr].sum() # for symmetric commands
+        if len(farr)>0:
+            return self.info.joint_maxpos[farr].sum() # for symmetric commands
+        else:
+            return 0
 
     def get_qpos(self):
         joint_states = self.sim.getJointStates(

gym-bullet-extensions/gym_bullet_extensions/envs/manipulator_env.py
@@ -127,23 +127,24 @@ class ManipulatorEnv(gym.Env):
         return rnd_params
 
     def set_randomize(self, rnd_params, debug=False):
-        assert(rnd_params.shape[0]==len(self.object_ids)*4)
-        rnd_params = rnd_params.reshape(len(self.object_ids), -1)
-        for objid in range(len(self.object_ids)):
-            mass = rnd_params[objid, 0]
-            restitution = rnd_params[objid, 1]
-            lat_fric = rnd_params[objid, 2]
-            rol_fric = rnd_params[objid, 3]
-            # TODO: Does bullet change inertial matrix based on mass, shape
-            # TODO: and type, or do we need to re-specify manually?
-            self.robot.sim.changeDynamics(
-                self.object_ids[objid], -1, mass=mass,
-                lateralFriction=lat_fric,
-                rollingFriction=rol_fric, restitution=restitution)
-            if debug:
-                msg = 'changeDynamics for obj {:d}: mass {:.4f}'
-                msg += ' restitution {:.4f} lat_fric {:.4f} roll_fric {:.4f}'
-                print(msg.format(objid, mass, restitution, lat_fric, rol_fric))
+        if len(self.object_ids) > 0:
+            assert(rnd_params.shape[0]==len(self.object_ids)*4)
+            rnd_params = rnd_params.reshape(len(self.object_ids), -1)
+            for objid in range(len(self.object_ids)):
+                mass = rnd_params[objid, 0]
+                restitution = rnd_params[objid, 1]
+                lat_fric = rnd_params[objid, 2]
+                rol_fric = rnd_params[objid, 3]
+                # TODO: Does bullet change inertial matrix based on mass, shape
+                # TODO: and type, or do we need to re-specify manually?
+                self.robot.sim.changeDynamics(
+                    self.object_ids[objid], -1, mass=mass,
+                    lateralFriction=lat_fric,
+                    rollingFriction=rol_fric, restitution=restitution)
+                if debug:
+                    msg = 'changeDynamics for obj {:d}: mass {:.4f}'
+                    msg += ' restitution {:.4f} lat_fric {:.4f} roll_fric {:.4f}'
+                    print(msg.format(objid, mass, restitution, lat_fric, rol_fric))
 
     def step(self, action):  # assume unscaled action
         action = np.clip(action, self.action_space.low, self.action_space.high)

gym-bullet-extensions/gym_bullet_extensions/envs/sawyer_env.py
@@ -22,9 +22,12 @@ class SawyerEnv(ManipulatorEnv):
             os.path.join('sawyer_robot', 'urdf', 'sawyer_bullet.urdf'),
             control_mode=control_mode,
             ee_joint_name='right_hand', ee_link_name='right_hand',
-            base_pos=[0,0,0],
-            rest_arm_qpos=rest_qpos,
-            visualize=visualize)
+            base_pos=[0, 0, 0.0], dt=1.0 / 240.0,
+            kp=([200.0] * 7 + [1.0] * 2),
+            kd=([2.0] * 7 + [0.1] * 2),
+            min_z=0.00,
+            visualize=visualize,
+            rest_arm_qpos=rest_qpos)
         assert(num_objects<=2)
         table_minmax_x_minmax_y = np.array([0.15, 0.77, -0.475, 0.475])
         super(SawyerEnv, self).__init__(